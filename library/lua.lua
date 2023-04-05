6eb64a50a71b0829400dbec94b9b7801bc009ce7
Josef Friedrich <josef@friedrich.rocks>
2023-04-05 18:33:42 +0200

    Sync with https://github.com/Josef-Friedrich/LuaTeX_Lua-API/commit/f2448d780b685ce1faab5a38793abaf7264793ca

--- library/lua.lua
+++ library/lua.lua
@@ -1,4 +1,6 @@
 ---@meta
+---
+---[Type definition and documentation](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/blob/main/library/luatex/lua.lua) incomplete or incorrect? [Please contribute!](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/pulls)
 lua = {}
 
 ---
@@ -15,6 +17,8 @@ lua = {}
 ---* Corresponding C source code: [llualib.c#L416-L417](https://github.com/TeX-Live/luatex/blob/f52b099f3e01d53dc03b315e1909245c3d5418d3/source/texk/web2c/luatexdir/lua/llualib.c#L416-L417)
 ---
 ---@type string
+---
+---[Type definition and documentation](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/blob/main/library/luatex/lua.lua) incomplete or incorrect? [Please contribute!](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/pulls)
 lua.version = ""
 
 ---
@@ -65,10 +69,12 @@ lua.version = ""
 ---* Corresponding C source code: [llualib.c#L356-L360](https://github.com/TeX-Live/luatex/blob/f52b099f3e01d53dc03b315e1909245c3d5418d3/source/texk/web2c/luatexdir/lua/llualib.c#L356-L360)
 ---
 ---@return {[integer]: fun(slot: integer)}
+---
+---[Type definition and documentation](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/blob/main/library/luatex/lua.lua) incomplete or incorrect? [Please contribute!](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/pulls)
 function lua.get_functions_table() end
 
 ---
----Returns a number indicating
+---Return a number indicating
 ---how much nesting is going on.
 ---
 ---It is only of use as a breakpoint when
@@ -80,11 +86,15 @@ function lua.get_functions_table() end
 ---* Corresponding C source code: [llualib.c#L370-L374](https://github.com/TeX-Live/luatex/blob/f52b099f3e01d53dc03b315e1909245c3d5418d3/source/texk/web2c/luatexdir/lua/llualib.c#L370-L374)
 ---
 ---@return integer
+---
+---[Type definition and documentation](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/blob/main/library/luatex/lua.lua) incomplete or incorrect? [Please contribute!](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/pulls)
 function lua.getstacktop() end
 
 ---
 ---Return a number indicating
----how much nesting is going on. It is only of use as a breakpoint when
+---how much nesting is going on.
+---
+---It is only of use as a breakpoint when
 ---checking some mechanism going haywire.
 ---
 ---* Corresponding C source code: [llualib.c#L376-L380](https://github.com/TeX-Live/luatex/blob/f52b099f3e01d53dc03b315e1909245c3d5418d3/source/texk/web2c/luatexdir/lua/llualib.c#L376-L380)
@@ -94,40 +104,112 @@ function lua.getstacktop() end
 ---* `LuaTeX` manual: 10.1.4 Introspection
 ---
 ---@return integer
+---
+---[Type definition and documentation](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/blob/main/library/luatex/lua.lua) incomplete or incorrect? [Please contribute!](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/pulls)
 function lua.getcalllevel() end
+
+---
+---Use the `bytecode` table to store *Lua* code chunks. The accepted values for
+---assignments are functions and `nil`. Likewise, the retrieved value is
+---either a function or `nil`.
+---
+---The contents of the `lua.bytecode` array is stored inside the format file
+---as actual *Lua* bytecode, so it can also be used to preload *Lua* code. The
+---function must not contain any upvalues.
+---@type table<integer, function|nil>
+---
+---[Type definition and documentation](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/blob/main/library/luatex/lua.lua) incomplete or incorrect? [Please contribute!](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/pulls)
+lua.bytecode = {}
 
 ---
 ---__Reference:__
 ---
 ---* Corresponding C source code: [llualib.c#L249-L315](https://github.com/TeX-Live/luatex/blob/f52b099f3e01d53dc03b315e1909245c3d5418d3/source/texk/web2c/luatexdir/lua/llualib.c#L249-L315)
-function lua.setbytecode() end
+---
+---@param n integer
+---@param f function|nil
+---
+---[Type definition and documentation](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/blob/main/library/luatex/lua.lua) incomplete or incorrect? [Please contribute!](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/pulls)
+function lua.setbytecode(n, f) end
 
 ---
 ---__Reference:__
 ---
 ---* Corresponding C source code: [llualib.c#L187-L212](https://github.com/TeX-Live/luatex/blob/f52b099f3e01d53dc03b315e1909245c3d5418d3/source/texk/web2c/luatexdir/lua/llualib.c#L187-L212)
+---
+---@return function|nil f
+---
+---[Type definition and documentation](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/blob/main/library/luatex/lua.lua) incomplete or incorrect? [Please contribute!](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/pulls)
 function lua.getbytecode() end
 
+---
+---Return two numbers, one for the command handler and one for the graphical user interface (on Microsoft Windows).
 ---
 ---__Reference:__
 ---
 ---* Corresponding C source code: [llualib.c#L382-L392](https://github.com/TeX-Live/luatex/blob/f52b099f3e01d53dc03b315e1909245c3d5418d3/source/texk/web2c/luatexdir/lua/llualib.c#L382-L392)
+---
+---@return integer # command handler
+---@return integer # graphical user interface
+---
+---[Type definition and documentation](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/blob/main/library/luatex/lua.lua) incomplete or incorrect? [Please contribute!](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/pulls)
 function lua.getcodepage() end
 
 ---
+---There is an array of 65536 (0--65535) potential chunk names for use with the
+---`directlua` and `latelua` primitives.
+---
+---```
+---lua.name[<number> n] = <string> s
+---<string> s = lua.name[<number> n]
+---```
+---
+---@type table<integer, string>
+---
+---[Type definition and documentation](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/blob/main/library/luatex/lua.lua) incomplete or incorrect? [Please contribute!](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/pulls)
+lua.name = {}
+
+---
+---Set a Lua chunk name.
+---
 ---__Reference:__
 ---
----* Corresponding C source code: [llualib.c#L318-L339](https://github.com/TeX-Live/luatex/blob/f52b099f3e01d53dc03b315e1909245c3d5418d3/source/texk/web2c/luatexdir/lua/llualib.c#L318-L339)
-function lua.setluaname() end
+---* Corresponding C source code: [llualib.c#L318-L339](https://github.com/TeX-Live/luatex/blob/f52b099f3e01d53dc03b315e1909245c3d5418d3/source/texk/web2c/luatexdir/lua/llualib.c#L318-L339)-
+---
+---@param s string|nil # If you want to unset a *Lua* name, you can assign `nil` to it.
+---@param n integer
+---
+---[Type definition and documentation](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/blob/main/library/luatex/lua.lua) incomplete or incorrect? [Please contribute!](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/pulls)
+function lua.setluaname(s, n) end
 
+---
+---Return a Lua chunk name.
 ---
 ---__Reference:__
 ---
 ---* Corresponding C source code: [llualib.c#L341-L354](https://github.com/TeX-Live/luatex/blob/f52b099f3e01d53dc03b315e1909245c3d5418d3/source/texk/web2c/luatexdir/lua/llualib.c#L341-L354)
-function lua.getluaname() end
+---
+---@param n number
+---
+---@return string|nil
+---
+---[Type definition and documentation](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/blob/main/library/luatex/lua.lua) incomplete or incorrect? [Please contribute!](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/pulls)
+function lua.getluaname(n) end
 
+---
+---Create a new empty table and push it onto the stack.
+---
+---Parameter `index` is a hint for how many elements the table will have as a sequence; parameter `hash` is a hint for how many other elements the table will have. Lua may use these hints to preallocate memory for the new table. This preallocation is useful for performance when you know in advance how many elements the table will have.
 ---
 ---__Reference:__
 ---
 ---* Corresponding C source code: [llualib.c#L362-L368](https://github.com/TeX-Live/luatex/blob/f52b099f3e01d53dc03b315e1909245c3d5418d3/source/texk/web2c/luatexdir/lua/llualib.c#L362-L368)
-function lua.newtable() end
+---* [lua_createtable](https://pgl.yoyo.org/luai/i/lua_createtable)-
+---
+---@param index integer
+---@param hash integer
+---
+---@return table
+---
+---[Type definition and documentation](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/blob/main/library/luatex/lua.lua) incomplete or incorrect? [Please contribute!](https://github.com/Josef-Friedrich/LuaTeX_Lua-API/pulls)
+function lua.newtable(index, hash) end
